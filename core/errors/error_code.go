f0090c82235a27a3904d3f88fd3ea4f820da4033
Peeranut <nutteen@gmail.com>
2022-11-02 18:23:54 +0700

    [core-lib] Added common error models and codes

---
+++ core/errors/error_code.go
@@ -0,0 +1,8 @@
+package errors
+
+var (
+	GENERIC_ERROR = NewErrorCode("G00001", "Generic exception")
+	BAD_REQUEST_BODY_PARSE_ERROR = NewErrorCode("R00001", "Request can't be parsed")
+	BAD_REQUEST_VALDATION_ERROR = NewErrorCode("R00002", "Request doesn't pass validation")
+	NOT_FOUND_ERROR = NewErrorCode("G00003", "Data not found exception")
+)
---
+++ core/errors/error_model.go
@@ -0,0 +1,50 @@
+package errors
+
+type ErrorResponseModel struct {
+	Error ErrorModel `json:"error"`
+}
+
+type ErrorModel struct {
+	Code string `json:"code"`
+	Message string `json:"message"`
+	DetailMessage string `json:"detailMessage"`
+}
+
+type ErrorCode struct {
+	Code string
+	Message string
+}
+
+func NewErrorCode(code string, message string) ErrorCode {
+	return ErrorCode{
+		Code: code,
+		Message: message,
+	}
+}
+
+func newShortErrorModel(errorCode ErrorCode) ErrorModel {
+	return ErrorModel{
+		Code: errorCode.Code,
+		Message: errorCode.Message,
+	}
+}
+
+func newErrorModel(errorCode ErrorCode, detailMessage string) ErrorModel {
+	return ErrorModel{
+		Code: errorCode.Code,
+		Message: errorCode.Message,
+		DetailMessage: detailMessage,
+	}
+}
+
+func NewErrorResponseModel(errorCode ErrorCode, detailMessage string) *ErrorResponseModel {
+	return &ErrorResponseModel{
+		Error: newErrorModel(errorCode, detailMessage),
+	}
+}
+
+func NewShortErrorResponseModel(errorCode ErrorCode) *ErrorResponseModel {
+	return &ErrorResponseModel{
+		Error: newShortErrorModel(errorCode),
+	}
+}

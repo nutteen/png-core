bfb8cb27fb8a621f34301c78b602cb956cd65fde
Peeranut <nutteen@gmail.com>
2022-11-06 16:56:08 +0700

    [core] Error, Refactored error models

--- core/utils/errorutils/error_code.go
+++ core/utils/errorutils/error_code.go
@@ -1,8 +1,8 @@
 package errorutils
 
 var (
-	GENERIC_ERROR = NewErrorCode("G00001", "Generic exception")
-	BAD_REQUEST_BODY_PARSE_ERROR = NewErrorCode("R00001", "Request can't be parsed")
-	BAD_REQUEST_VALDATION_ERROR = NewErrorCode("R00002", "Request doesn't pass validation")
-	NOT_FOUND_ERROR = NewErrorCode("G00003", "Data not found exception")
+	UNKNOWN_ERROR = NewErrorCode("G99999", "UNKNOWN_ERROR", "Unknown exception", 500)
+	BAD_REQUEST_BODY_PARSE_ERROR = NewErrorCode("G00001", "REQUEST_PARSE_ERROR", "Request parse exception", 400)
+	BAD_REQUEST_VALDATION_ERROR = NewValidationErrorCode("G00002", "REQUEST_VALIDATION_ERROR", "Request validation exception", 400)
+	NOT_FOUND_ERROR = NewErrorCode("G00003", "DATA_NOT_FOUND_ERROR","Data not found exception", 404)
 )
--- core/utils/errorutils/error_model.go
+++ core/utils/errorutils/error_model.go
@@ -1,50 +1,100 @@
 package errorutils
 
+func NewErrorCode(code string, errorType string, title string, status int) *ErrorCode {
+	return &ErrorCode{
+		Code: code,
+		Type: errorType,
+		Title: title,
+		Status: status,
+	}
+}
+
+// ErrorCode predefines code, type and title to be used to construct ErrorResponseModel
+type ErrorCode struct {
+	Code string
+	Type string
+	Title string
+	Status int
+}
+
+func (e ErrorCode) NewErrorResponseModel(errorDetail string) *ErrorResponseModel {
+	return &ErrorResponseModel{
+		Error: ErrorModel{
+			Code: e.Code,
+			Type: e.Type,
+			Title: e.Title,
+			Status: e.Status,
+			Detail: errorDetail,
+		},
+	}
+}
+
 type ErrorResponseModel struct {
 	Error ErrorModel `json:"error"`
 }
 
+// ErrorModel implements https://www.rfc-editor.org/rfc/rfc7807
 type ErrorModel struct {
+	// an error code that is numeric presentation of the error type
 	Code string `json:"code"`
-	Message string `json:"message"`
-	DetailMessage string `json:"detailMessage"`
+	// error type
+	Type string `json:"type"`
+	// a short, human-readable summary of the problem type
+	Title string `json:"title"`
+	// http status code
+	Status int `json:"status"`
+	// a human-readable explanation specific to this occurrence of the problem
+	Detail string `json:"detail"`
+	// A URI reference that identifies the specific resource that cause the problem
+	Instance string `json:"Instance"`
 }
 
-type ErrorCode struct {
-	Code string
-	Message string
+type ValidationErrorResponseModel struct {
+	Error ValidationErrorModel `json:"error"`
 }
 
-func NewErrorCode(code string, message string) ErrorCode {
-	return ErrorCode{
+type ValidationErrorModel struct {
+	// Base error model
+	ErrorModel ErrorModel
+	// Extension members
+	InvalidParams []ValidationInvalidParam `json:"invalidParams"`
+}
+
+type ValidationInvalidParam struct {
+	// Name of the invalid attribute
+	Name string
+	// Invalid reason of the attribute
+	Reason string
+}
+
+func NewValidationErrorCode(code string, errorType string, title string, status int) *ValidationErrorCode {
+	return &ValidationErrorCode{
 		Code: code,
-		Message: message,
+		Type: errorType,
+		Title: title,
+		Status: status,
 	}
 }
 
-func newShortErrorModel(errorCode ErrorCode) ErrorModel {
-	return ErrorModel{
-		Code: errorCode.Code,
-		Message: errorCode.Message,
-	}
+// ValidationErrorCode predefines code, type and title to be used to construct ValidationErrorResponseModel
+type ValidationErrorCode struct {
+	Code string
+	Type string
+	Title string
+	Status int
 }
 
-func newErrorModel(errorCode ErrorCode, detailMessage string) ErrorModel {
-	return ErrorModel{
-		Code: errorCode.Code,
-		Message: errorCode.Message,
-		DetailMessage: detailMessage,
-	}
-}
-
-func NewErrorResponseModel(errorCode ErrorCode, detailMessage string) *ErrorResponseModel {
-	return &ErrorResponseModel{
-		Error: newErrorModel(errorCode, detailMessage),
-	}
-}
-
-func NewShortErrorResponseModel(errorCode ErrorCode) *ErrorResponseModel {
-	return &ErrorResponseModel{
-		Error: newShortErrorModel(errorCode),
+func (e ValidationErrorCode) NewErrorResponseModel(errorDetail string, invalidParams []ValidationInvalidParam) *ValidationErrorResponseModel {
+	return &ValidationErrorResponseModel{
+		Error: ValidationErrorModel{
+			ErrorModel: ErrorModel{
+				Code: e.Code,
+				Type: e.Type,
+				Title: e.Title,
+				Status: e.Status,
+				Detail: errorDetail,
+			},
+			InvalidParams: invalidParams,
+		},
 	}
 }
